df0f153a1be6e5fea16095ee363f074ae1fcaf89
Edvaldo Vitor De Castro Souza <116117189+edvaldovitor250@users.noreply.github.com>
2025-08-12 15:35:37 -0300

    feat: add Spring Boot project setup

---
+++ src/main/java/com/mv/financeiro_controladoria/FinanceiroControladoriaApplication.java
@@ -0,0 +1,13 @@
+package com.mv.financeiro_controladoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroControladoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroControladoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mv/financeiro_controladoria/FinanceiroControladoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mv.financeiro_controladoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceiroControladoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
